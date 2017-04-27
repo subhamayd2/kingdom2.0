d390f1e991148d5d51404f48358905b7661522bd
subhamayd2 <bloodyshower@gmail.com>
2017-04-27 17:16:40 +0530

    About page

---
+++ js/global.js
@@ -0,0 +1,5 @@
+$('ul.nav li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).slideDown(300);
+}, function () {
+    $(this).find('.dropdown-menu').stop(true, true).delay(100).slideUp(300);
+});
---
+++ js/scrollreveal.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+sr.reveal('.agile-header', { duration: 1500, origin: 'top', scale: 1, container: '.container' });
+sr.reveal('.navbar', { duration: 1500, origin: 'bottom', scale: 1, delay: 500, distance: '50px' });
+sr.reveal('.w3ls_head', { duration: 1500, origin: 'top', scale: 1, reset: true });
+sr.reveal('.w3ls_head + p', { duration: 1500, origin: 'bottom', reset: true });
+sr.reveal('.chrt_grid', { duration: 1500, origin: 'bottom', reset: true }, 50);
+sr.reveal('.w3ls_courses_left_grid h3', { duration: 1500, origin: 'bottom', reset: true, delay: 500 }, 50);
+sr.reveal('.w3ls_courses_left_grid p', { duration: 1500, origin: 'right', reset: true, delay: 800 }, 50);
+sr.reveal('.w3ls_courses_left_grid img', { duration: 1500, origin: 'left', reset: true, delay: 800 }, 50);
+sr.reveal('.agileinfo_choose_right .col-sm-6', { duration: 1500, origin: 'bottom', reset: true }, 50);
+
